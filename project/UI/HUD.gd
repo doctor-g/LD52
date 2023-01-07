95fffd9c8d832d33cc97a9abea1a64dfc38ccc3d
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-01-07 09:53:57 -0500

    Lerp the score

--- project/UI/HUD.gd
+++ project/UI/HUD.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var _displayed_score := 0.0
 
 func _process(_delta):
-	$ScoreLabel.text = str(Globals.score)
+	_displayed_score = lerp(_displayed_score, Globals.score, 0.4)
+	$ScoreLabel.text = str(int(ceil(_displayed_score)))
